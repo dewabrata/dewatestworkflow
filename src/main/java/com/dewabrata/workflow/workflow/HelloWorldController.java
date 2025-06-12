4236edfc7286c2000c07e5e79b0dad493dcd3adf
dewabrata <dewabrata@gmail.com>
2025-06-12 11:18:10 +0700

    Inisialisasi Project

---
+++ src/main/java/com/dewabrata/workflow/workflow/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.dewabrata.workflow.workflow;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World! Welcome to CI/CD with GitHub Actions!";
+    }
+}
+
